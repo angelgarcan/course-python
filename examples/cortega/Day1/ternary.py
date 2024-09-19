bebf7002d2edc162445d3ba2e9653a97357c06e1
Ivan Ortega <christian-ortega@live.com>
2024-09-19 12:05:19 -0700

    addinh ternary

---
+++ examples/cortega/Day1/ternary.py
@@ -0,0 +1,13 @@
+num = 7
+
+# Using ternary operator to check if the number is even or odd
+result = "Even" if num % 2 == 0 else "Odd"
+
+print(result)  # Output: Odd
+
+age = 18
+
+# Using ternary operator to determine if someone is an adult
+status = "Adult" if age >= 18 else "Minor"
+
+print(status)  # Output: Adult
